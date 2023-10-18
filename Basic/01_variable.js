71366202f021f2632596206daf074520c26a4637
anandcoder2006 <114492116+anandcoder2006@users.noreply.github.com>
2023-10-18 07:38:38 +0000

    update

---
+++ Basic/01_variable.js
@@ -0,0 +1,12 @@
+const userName="Anand"
+let userEmail="ad@gmail.com"
+var userPass="123445"
+userCity="Lucknow"
+
+
+//userName="Verma"
+userEmail="ds@gmail.com"
+userPass="3434343"
+userCity="GKP"
+
+console.table([userName, userEmail, userPass,userCity])
